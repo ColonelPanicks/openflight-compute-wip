1445f90dc263039c8abb18630f171ac98a693175
stuts <stu.j.franks@gmail.com>
2019-08-08 12:10:30 +0100

    Initial commit

---
+++ architect-scripts/flightdirect.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+#FLIGHTdescription: Install Flight Direct
+#FLIGHTstages: second
+
+VERSION=2.1.4
+
+unset RUBYLIB RUBYOPT
+unset $(env | grep ^BUNDLE | cut -f1 -d=)
+
+curl -L https://raw.githubusercontent.com/alces-software/flight-direct/master/scripts/bootstrap.sh | bash -s $VERSION
+source /etc/profile
+
+unset RUBYLIB RUBYOPT
+unset $(env | grep ^BUNDLE | cut -f1 -d=)
+
+<% if (node.config.gateway rescue false) -%>
+ROLE=login
+<% else -%>
+ROLE=compute
+<% end -%>
+
+flight config set role=$ROLE clustername=<%= config.cluster %>
+
+flight forge install flight-$ROLE
+
+<% if (node.config.gateway rescue false) -%>
+# Enable sessions
+flight session enable base/gnome
+<% end -%>
+
+# Disable user gridware
+sed -i 's/.*cw_GRIDWARE_allow_users=.*/cw_GRIDWARE_allow_users=false/g' /opt/flight-direct/etc/gridware.rc
+
+# Enable storage types
+flight storage enable base/s3
---
+++ do-it.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#
+# Run this script on a hub
+#
+
+# TODO: Ensure enough disk space on gateway to support running jobs, etc
+# TODO: Ensure a genders file exists
+# TODO: Setup user with password & SSH key
+
+#################
+# Checking Args #
+#################
+
+CLUSTERNAME="$1"
+ROOTPASS="$2"
+SSH_PUB_KEY="$3"
+
+if [ -z "${CLUSTERNAME}" ] ; do
+    echo "Provide cluster name"
+    echo "  do-it.sh CLUSTERNAME ROOTPASS SSH_PUB_KEY"
+    exit 1
+elif [ -z "${ROOTPASS}" ] ; do 
+    echo "Provide root password"
+    echo "  do-it.sh CLUSTERNAME ROOTPASS SSH_PUB_KEY"
+    exit 1
+elif [ -z "${SSH_PUB_KEY}" ] ; do 
+    echo "Provide ssh public key"
+    echo "  do-it.sh CLUSTERNAME ROOTPASS SSH_PUB_KEY"
+    exit 1
+fi
+
+###########################
+# Keep Plugins Up-To-Date #
+###########################
+
+# Put plugins in place
+if ! [ -d /tmp/flight-architect ] ; then
+    git clone https://github.com/openflighthpc/flight-architect /tmp/flight-architect
+fi
+cd /tmp/flight-architect
+git checkout feature/plugins
+git pull
+rsync -auv /tmp/flight-architect/data/ /opt/flight/opt/architect/data/
+
+######################
+# Create New Cluster #
+######################
+
+# Initialise cluster
+set +m # Silence background job creation message
+{ flight architect cluster init $CLUSTERNAME > /dev/null & } 2>/dev/null
+PID=$!
+sleep 5
+kill -9 $PID 2> /dev/null
+set -m # Enable background job creation message
+
+# Configure domain
+flight architect configure domain -a "{ \"cluster_name\": \"$CLUSTERNAME\", \"root_password\": \"$(openssl passwd -1 $ROOTPASS)\", \"root_ssh_key\": \"$SSH_PUB_KEY\", \"network2_defined\": false, \"network3_defined\": false }"
+
+# Generate Templates
+flight architect template
+cp /var/lib/underware/clusters/$CLUSTERNAME/var/rendered/kickstart/domain/platform/manifest.yaml /var/lib/underware/clusters/$CLUSTERNAME/var/rendered/
+
+# Import to cloud
+flight cloud cluster init $CLUSTERNAME aws
+flight cloud import /var/lib/underware/clusters/$CLUSTER/var/rendered/manifest.yaml > /dev/null
+
+####################
+# Deploy Resources #
+####################
+
+# Deploy domain/gateway
+flight cloud deploy domain && flight cloud deploy gateway1
+
+# Deploy nodes
+
