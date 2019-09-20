9dd306d2091056a236630aec6d8bc98b93573bd9
stuts <stu.j.franks@gmail.com>
2019-09-20 14:37:09 +0100

    Support Cloud 2.1.0

--- build-cluster.sh
+++ build-cluster.sh
@@ -29,18 +29,22 @@ fi
 # Import to Cloud #
 ###################
 flight cloud cluster init $CLUSTERNAME $PLATFORM
-flight cloud import /var/lib/architect/clusters/$TYPE/var/rendered/manifest.yaml > /dev/null
+flight cloud domain create /var/lib/architect/clusters/$TYPE/var/rendered/$PLATFORM/domain/platform/domain.* > /dev/null
+flight cloud node create gateway1 /var/lib/architect/clusters/$TYPE/var/rendered/$PLATFORM/node/gateway1/platform/gateway1.*
+flight cloud node create node01 /var/lib/architect/clusters/$TYPE/var/rendered/$PLATFORM/node/node01/platform/node01.*
+flight cloud node create node02 /var/lib/architect/clusters/$TYPE/var/rendered/$PLATFORM/node/node02/platform/node02.*
+
+flight cloud group create nodes
+flight cloud group add -p nodes node01 node02
 
 ####################
 # Deploy Resources #
 ####################
 
 # Deploy domain/gateway
-flight cloud deploy domain && flight cloud deploy gateway1 -p "securitygroup,network1SubnetID=*domain nametext=$CLUSTERNAME user_ssh_pub_key='$SSH_PUB_KEY'"
-
-# Allow enought time for Direct to be setup on gateway
-sleep 180
+flight cloud domain deploy
+flight cloud node deploy gateway1 "securitygroup,network1SubnetID=*domain nametext=$CLUSTERNAME user_ssh_pub_key='$SSH_PUB_KEY'"
 
 # Deploy nodes
-flight cloud deploy node01 -p "securitygroup,network1SubnetID=*domain nametext=$CLUSTERNAME user_ssh_pub_key='$SSH_PUB_KEY'"
-flight cloud deploy node02 -p "securitygroup,network1SubnetID=*domain nametext=$CLUSTERNAME user_ssh_pub_key='$SSH_PUB_KEY'"
+flight cloud node deploy node01 "securitygroup,network1SubnetID=*domain nametext=$CLUSTERNAME user_ssh_pub_key='$SSH_PUB_KEY'"
+flight cloud node deploy node02 "securitygroup,network1SubnetID=*domain nametext=$CLUSTERNAME user_ssh_pub_key='$SSH_PUB_KEY'"
