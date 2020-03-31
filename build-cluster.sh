6f50a84807dfa9ebb7e44bd7fe62c70615f762b1
stuts <stu.j.franks@gmail.com>
2020-03-31 13:50:40 +0100

    Stop SSH from swallowing stdin

--- build-cluster.sh
+++ build-cluster.sh
@@ -224,11 +224,10 @@ function set_hostnames() {
         name=$(echo "$node" |awk '{print $1}')
         ip=$(echo "$node" |awk '{print $2}' |sed 's/.*ansible_host=//g')
 
-        until ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ip exit 2>/dev/null ; do
-            echo "Waiting for $name to be reachable"
+        until ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ip exit </dev/null 2>/dev/null ; do
             sleep 5
         done
-        ssh $ip "hostnamectl set-hostname $name.pri.$CLUSTERNAMEARG.cluster.local"
+        ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ip "hostnamectl set-hostname $name.pri.$CLUSTERNAMEARG.cluster.local" </dev/null
     done <<< "$(echo "$NODES")"
 }
 
